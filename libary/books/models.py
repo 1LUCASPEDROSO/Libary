e88fd51bd86f50b46d15f007af1200aa52cc1286
lucas pedroso <pedrosolucas1745@gmail.com>
2024-06-26 18:19:41 -0300

    add models
    add  models category and book

--- libary/books/models.py
+++ libary/books/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
+class Category(models.Model):
+    name = models.CharField(max_length= 100, null=False, blank=False)
+    def __str__(self):
+        return self.name
 
+class Book(models.Model):
+    title = models.CharField(max_length=200, null=False, blank=True)
+    description = models.TextField()
+    author = models.CharField(max_length= 200)
+    release_date = models.CharField(max_length=12 ,null=True, blank=True)
+    categorys = models.ManyToManyField(Category, related_name='books')
+
+    def __str__(self):
+        return self.title
